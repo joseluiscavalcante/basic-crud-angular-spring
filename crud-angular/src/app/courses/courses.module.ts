67146b095d74b83b62ec83adb248924f29d59faa
José Cavalcante <joseluismoreiracavalcante@gmail.com>
2023-12-12 14:17:27 -0300

    Adiciona lógica do formulário

--- crud-angular/src/app/courses/courses.module.ts
+++ crud-angular/src/app/courses/courses.module.ts
@@ -1,20 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ImportsMaterialModule } from '../shared/imports-material/imports-material.module';
 import { SharedModule } from '../shared/shared.module';
 import { CoursesRoutingModule } from './courses-routing.module';
-import { CoursesComponent } from './courses/courses.component';
+import { CoursesComponent } from './courses/courses.component';  // Módulo necessário para utilizar FormBuilder e FormGroup
+import { CourseFormComponent } from './course-form/course-form.component';
 
 @NgModule({
   declarations: [
-    CoursesComponent
+    CoursesComponent,
+    CourseFormComponent
   ],
   imports: [
     CommonModule,
     CoursesRoutingModule,
     ImportsMaterialModule,
-    SharedModule
+    SharedModule,
+    ReactiveFormsModule
   ]
 })
 export class CoursesModule { }
