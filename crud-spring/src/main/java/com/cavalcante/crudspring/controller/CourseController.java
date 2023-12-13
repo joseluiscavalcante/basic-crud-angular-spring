406227c5c44ac93378c7f0c152c3e923d45f86c8
José Cavalcante <joseluismoreiracavalcante@gmail.com>
2023-12-13 13:33:35 -0300

    Inclui método para salvar registro no BD

--- crud-spring/src/main/java/com/cavalcante/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/cavalcante/crudspring/controller/CourseController.java
@@ -2,8 +2,13 @@ package com.cavalcante.crudspring.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cavalcante.crudspring.model.Course;
@@ -21,5 +26,12 @@ public class CourseController {
   @GetMapping  // @RequestMapping(method = RequestMethod.GET)
   public List<Course> list() {
     return courseRepository.findAll();  // Realiza um "SELECT *" na tabela
+  }
+
+  @PostMapping  // @RequestMapping(method = RequestMethod.POST)
+  @ResponseStatus(code = HttpStatus.CREATED)  // Altera o status http da resposta para 201 - "A solicitação foi atendida, resultando na criação de um novo recurso".
+  public Course create (@RequestBody Course course) {
+    // Salva e retorna o valor recebido
+    return courseRepository.save(course);
   }
 }
