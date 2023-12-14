89261784e601f08361e50b3c055049c213b22dfd
José Cavalcante <joseluismoreiracavalcante@gmail.com>
2023-12-14 14:29:29 -0300

    Inicia método de buscar pelo ID

--- crud-spring/src/main/java/com/cavalcante/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/cavalcante/crudspring/controller/CourseController.java
@@ -1,10 +1,12 @@
 package com.cavalcante.crudspring.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,13 +25,22 @@ public class CourseController {
 
   private final CourseRepository courseRepository;  // Considerado como final para garantir que não haverá nenhuma alteração dessa instância.
   
-  @GetMapping  // @RequestMapping(method = RequestMethod.GET)
+  @GetMapping  // ~~@RequestMapping(method = RequestMethod.GET)
   public List<Course> list() {
     return courseRepository.findAll();  // Realiza um "SELECT *" na tabela
   }
 
-  @PostMapping  // @RequestMapping(method = RequestMethod.POST)
+  // Complementa a URL da API, permitindo que seja especificado um ID para retornar apenas 1 objeto
+  @GetMapping("/{id}")
+  // PathVariable: Indica que estamos capturando o valor do id passado no caminho da API
+  public Optional<Course> findById(@PathVariable("id") Long id) {
+    return courseRepository.findById(id);
+    
+  }
+
+  @PostMapping  // ~~@RequestMapping(method = RequestMethod.POST)
   @ResponseStatus(code = HttpStatus.CREATED)  // Altera o status http da resposta para 201 - "A solicitação foi atendida, resultando na criação de um novo recurso".
+  // @RequestBody --> indica que um controller method deve esperar o body da requisição HTTP para ser convertido automaticamente em um objeto Java correspondente.
   public Course create (@RequestBody Course course) {
     // Salva e retorna o valor recebido
     return courseRepository.save(course);
